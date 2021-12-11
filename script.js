97bd928f7dfbb47e31537518f9940ba09e23d6cb
alexndra1272 <alexandrag1272@gmail.com>
2021-12-11 12:13:31 -0600

    p

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const navToggle = document.querySelector(".nav-toggle");
 const navMenu = document.querySelector(".lista");
 
-navToggle.addEventListener("click", () => {
+navToggle.addListener("click", () => {
   navMenu.classList.toggle("visible-m");
 });
